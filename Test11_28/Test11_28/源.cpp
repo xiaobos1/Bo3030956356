844e836d5e0c22a77bfedc86b30eb5b42fcf0eb7
xiaobo <3030956356@qq.com>
2022-11-28 20:18:34 +0800

    练习

---
+++ Test11_28/Test11_28/源.cpp
@@ -0,0 +1,172 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int num = 19;
+//int main()
+//{
+//	int num = 1;
+//	printf("结果是:%d\n", num);
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//	char arr[] = { 'a','b','c' };
+//	printf("%d", strlen(arr));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int sum = 0;
+//	int j, c = 1;;
+//	for (int i = 1; i <= 5; i++) {
+//		c = 1;
+//		for (j = 1; j <= i; j++)
+//			c *= j;
+//		sum += c;
+//	}
+//	printf("%d\n",c);
+//	return 0;
+//}
+
+//int func(int n)
+//{
+//	if (n == 1)
+//		return 1;
+//	return n * func(n - 1);
+//}
+//
+//int main()
+//{
+//	int f = func(5);
+//	printf("%d", f);
+//	return 0;
+//}
+
+//void reverse(char* ch)
+//{
+//	if(ch==NULL)
+//
+//	else
+//}
+
+//void f(int n)
+//{
+//	char c;
+//	if (n == 1)
+//	{
+//		c = getchar();
+//		putchar(c);
+//	}
+//	else
+//	{
+//		
+//		c = getchar();
+//		f(n - 1);
+//		putchar(c);
+//	}
+//}
+//
+//int main()
+//{
+//	void f(int n);
+//	f(5);
+//	return 0;
+//}
+
+//int age(int n)
+//{
+//	int c = 0;
+//	if (n == 1)
+//		c = 10;
+//	else
+//		c = age(n - 1) + 2;
+//	return c;
+//}
+//
+//int main()
+//{
+//	printf("%d", age(5));
+//	return 0;
+//}
+
+
+////
+//int fun(int n)
+//{
+//	static int c = 0;
+//	
+//	if (n == 0)
+//		return c;
+//
+//	c++;
+//	return fun(n / 10);
+//}
+//void reverse(int n)
+//{
+//	
+//	if (n == 0)
+//		return;
+//
+//	int b = n % 10;
+//	printf("%d", b);
+//	reverse(n / 10);
+//}
+//int main()
+//{
+//	int n = 0,m=0;
+//	scanf("%d", &n);
+//	printf("长度：%d\n", fun(n));
+//	printf("反转：");
+//	reverse(n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char c,k;
+//	c = 't';
+//	switch (c)
+//	{
+//	case 'm':
+//		printf("Monday\n");
+//		break;
+//	case 'w':
+//		printf("Wednesday\n");
+//		break;
+//	case 't':
+//		printf("请你在输入下一位：\n");
+//		scanf("%c", &k);
+//		if (k == 'u')
+//			printf("tuesday\n");
+//		else if (k == 'h')
+//			printf("thursday\n");
+//		break;
+//	default:
+//		break;
+//	}
+//	return 0;
+//}
+#include <string.h>
+//int main()
+//{
+//	char str[] = "hello world";
+//
+//	char* ps = str;
+//	for (int i = 0; i < strlen(str); i++)
+//	{
+//		if (str[i] == ' ')
+//			str[i] = '%';
+//	}
+//	printf("%s", str);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
